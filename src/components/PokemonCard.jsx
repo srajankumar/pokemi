a3023e6e5dd5aa1260fcd01768f3bcf9721f4f67
srajan <kumarsrajan02@gmail.com>
2023-09-14 21:20:57 +0530

    image done.. :)

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,27 +1,3 @@
-// import React from "react";
-
-// const PokemonCard = ({ pokemon, index, onClick }) => {
-//   return (
-//     <div
-//       className="m-5 bg-green-400 w-40 flex justify-center items-center flex-col"
-//       onClick={onClick}
-//     >
-//       <img
-//         src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
-//           index + 1
-//         }.png`}
-//         alt={pokemon.name}
-//       />
-//       <p>{pokemon.name}</p>
-//       <p>{pokemon.index}</p>
-//     </div>
-//   );
-// };
-
-// export default PokemonCard;
-
-import React from "react";
-
 const PokemonCard = ({ pokemon, index, onClick }) => {
   return (
     <div
@@ -29,37 +5,13 @@ const PokemonCard = ({ pokemon, index, onClick }) => {
       onClick={onClick}
     >
       <img
-        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/showdown/${
+        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/${
           index + 1
         }.gif`}
         alt={pokemon.name}
       />
-      <img
-        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${
-          index + 1
-        }.png`}
-        alt={pokemon.name}
-      />
-      <img
-        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
-          index + 1
-        }.png`}
-        alt={pokemon.name}
-      />
-      <img
-        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${
-          index + 1
-        }.svg`}
-        alt={pokemon.name}
-      />
-      <img
-        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${
-          index + 1
-        }.png`}
-        alt={pokemon.name}
-      />
       <p>{pokemon.name}</p>
-      <p>{index + 1}</p> {/* Corrected to display the index */}
+      <p>{index + 1}</p>
     </div>
   );
 };
--- src/components/PokemonDetails.jsx
+++ src/components/PokemonDetails.jsx
@@ -1,9 +1,143 @@
-// PokemonDetails.js
+// import React, { useState, useEffect } from "react";
+// import axios from "axios";
+
+// const PokemonDetails = ({ pokemonName }) => {
+//   const [pokemonData, setPokemonData] = useState(null);
+
+//   useEffect(() => {
+//     axios
+//       .get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+//       .then((response) => {
+//         setPokemonData(response.data);
+//       });
+//   }, [pokemonName]);
+
+//   if (!pokemonData) {
+//     return <div>Loading...</div>;
+//   }
+
+//   return (
+//     <div className="fixed top-0 right-0 bg-black text-white">
+//       <h2>{pokemonData.name}</h2>
+//       <img src={pokemonData.sprites.front_default} alt={pokemonData.name} />
+//       <img
+//         src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/${pokemonData.id}.gif`}
+//         alt={pokemonData.name}
+//       />
+//       <img
+//         src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/back/${pokemonData.id}.gif`}
+//         alt={pokemonData.name}
+//       />
+//       <img
+//         src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/shiny/${pokemonData.id}.gif`}
+//         alt={pokemonData.name}
+//       />
+//       <img
+//         src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/back/shiny/${pokemonData.id}.gif`}
+//         alt={pokemonData.name}
+//       />
+//       <h3>Details:</h3>
+//       <p>ID: {pokemonData.id}</p>
+//       <p>Height: {pokemonData.height} decimetres</p>
+//       <p>Weight: {pokemonData.weight} hectograms</p>
+
+//       <h3>Abilities:</h3>
+//       <ul>
+//         {pokemonData.abilities.map((ability, index) => (
+//           <li key={index}>{ability.ability.name}</li>
+//         ))}
+//       </ul>
+
+//       <h3>Types:</h3>
+//       <ul>
+//         {pokemonData.types.map((type, index) => (
+//           <li key={index}>{type.type.name}</li>
+//         ))}
+//       </ul>
+
+//       {/* Add more details you want to display */}
+//     </div>
+//   );
+// };
+
+// export default PokemonDetails;
+
+// import React, { useState, useEffect } from "react";
+// import axios from "axios";
+
+// const PokemonDetails = ({ pokemonName }) => {
+//   const [pokemonData, setPokemonData] = useState(null);
+//   const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+//   useEffect(() => {
+//     axios
+//       .get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+//       .then((response) => {
+//         setPokemonData(response.data);
+//       });
+//   }, [pokemonName]);
+
+//   const images = [
+//     pokemonData && pokemonData.sprites.front_default,
+//     `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/${pokemonData?.id}.gif`,
+//     `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/back/${pokemonData?.id}.gif`,
+//     `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/shiny/${pokemonData?.id}.gif`,
+//     `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/back/shiny/${pokemonData?.id}.gif`,
+//   ];
+
+//   const handleNextImage = () => {
+//     setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+//   };
+
+//   const handlePrevImage = () => {
+//     setCurrentImageIndex(
+//       (prevIndex) => (prevIndex - 1 + images.length) % images.length
+//     );
+//   };
+
+//   if (!pokemonData) {
+//     return <div>Loading...</div>;
+//   }
+
+//   return (
+//     <div className="fixed top-0 right-0 bg-black text-white">
+//       <h2>{pokemonData.name}</h2>
+//       <img src={images[currentImageIndex]} alt={pokemonData.name} />
+//       <button onClick={handlePrevImage}>Previous</button>
+//       <button onClick={handleNextImage}>Next</button>
+
+//       <h3>Details:</h3>
+//       <p>ID: {pokemonData.id}</p>
+//       <p>Height: {pokemonData.height} decimetres</p>
+//       <p>Weight: {pokemonData.weight} hectograms</p>
+
+//       <h3>Abilities:</h3>
+//       <ul>
+//         {pokemonData.abilities.map((ability, index) => (
+//           <li key={index}>{ability.ability.name}</li>
+//         ))}
+//       </ul>
+
+//       <h3>Types:</h3>
+//       <ul>
+//         {pokemonData.types.map((type, index) => (
+//           <li key={index}>{type.type.name}</li>
+//         ))}
+//       </ul>
+
+//       {/* Add more details you want to display */}
+//     </div>
+//   );
+// };
+
+// export default PokemonDetails;
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const PokemonDetails = ({ pokemonName }) => {
   const [pokemonData, setPokemonData] = useState(null);
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const [showShinyImages, setShowShinyImages] = useState(false);
 
   useEffect(() => {
     axios
@@ -13,14 +147,73 @@ const PokemonDetails = ({ pokemonName }) => {
       });
   }, [pokemonName]);
 
+  const shinyImages = [
+    `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/shiny/${pokemonData?.id}.gif`,
+    `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/back/shiny/${pokemonData?.id}.gif`,
+  ];
+
+  const nonShinyImages = [
+    `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/${pokemonData?.id}.gif`,
+    `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/back/${pokemonData?.id}.gif`,
+  ];
+
+  const handleNextImage = () => {
+    setCurrentImageIndex(
+      (prevIndex) =>
+        (prevIndex + 1) %
+        (showShinyImages ? shinyImages.length : nonShinyImages.length)
+    );
+  };
+
+  const handlePrevImage = () => {
+    setCurrentImageIndex(
+      (prevIndex) =>
+        (prevIndex -
+          1 +
+          (showShinyImages ? shinyImages.length : nonShinyImages.length)) %
+        (showShinyImages ? shinyImages.length : nonShinyImages.length)
+    );
+  };
+
+  const toggleShinyImages = () => {
+    setShowShinyImages((prevShowShinyImages) => !prevShowShinyImages);
+  };
+
   if (!pokemonData) {
     return <div>Loading...</div>;
   }
 
+  const currentImages = showShinyImages ? shinyImages : nonShinyImages;
+
   return (
     <div className="fixed top-0 right-0 bg-black text-white">
       <h2>{pokemonData.name}</h2>
-      <img src={pokemonData.sprites.front_default} alt={pokemonData.name} />
+      <img src={currentImages[currentImageIndex]} alt={pokemonData.name} />
+      <button onClick={handlePrevImage}>Previous</button>
+      <button onClick={handleNextImage}>Next</button>
+      <button onClick={toggleShinyImages}>
+        {showShinyImages ? "Show Non-Shiny" : "Show Shiny"}
+      </button>
+
+      <h3>Details:</h3>
+      <p>ID: {pokemonData.id}</p>
+      <p>Height: {pokemonData.height} decimetres</p>
+      <p>Weight: {pokemonData.weight} hectograms</p>
+
+      <h3>Abilities:</h3>
+      <ul>
+        {pokemonData.abilities.map((ability, index) => (
+          <li key={index}>{ability.ability.name}</li>
+        ))}
+      </ul>
+
+      <h3>Types:</h3>
+      <ul>
+        {pokemonData.types.map((type, index) => (
+          <li key={index}>{type.type.name}</li>
+        ))}
+      </ul>
+
       {/* Add more details you want to display */}
     </div>
   );
